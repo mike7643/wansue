0ac03ca60a0330196e883a80bc79aa91a88bd6c3
Jung Yumin <developerjyumin@gmail.com>
2024-12-09 22:42:13 +0900

    첫 프로젝트 환경설정 완료

---
+++ src/main/java/com/wansue/wansue/WansueApplication.java
@@ -0,0 +1,13 @@
+package com.wansue.wansue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WansueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WansueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wansue/wansue/WansueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wansue.wansue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WansueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
